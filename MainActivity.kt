47f7f05258c40868335c371d163eb9eda53754f4
Lohith Duba <lohithduba@gmail.com>
2025-01-25 18:57:28 +0530

    MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.login
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val usernameEditText = findViewById<EditText>(R.id.etUsername)
+        val passwordEditText = findViewById<EditText>(R.id.etPassword)
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+
+        // Handle Login Button click
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                // Simulate successful login
+                Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                // Show error message
+                Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
